9691c379fc8e9e663a4205927c44dd2b9aab6307
bajiautomation <87237458+bajiautomation@users.noreply.github.com>
2021-10-21 23:40:30 +0530

    Modified file Commited

--- src/test/java/gitTest/test.java
+++ src/test/java/gitTest/test.java
@@ -3,7 +3,8 @@ package gitTest;
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Test class message");
+		System.out.println("Test class message 2s");
 
 	}
 
